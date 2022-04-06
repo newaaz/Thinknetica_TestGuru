ce7cdee06b7ddfe5bd3c8d2a00f2d569c4717be1
aaz <ilk-ui@yandex.ru>
2022-04-06 18:54:30 +0300

    add author to tests, delete admin from users

--- db/schema.rb
+++ db/schema.rb
@@ -39,16 +39,16 @@ ActiveRecord::Schema.define(version: 2022_04_05_120532) do
     t.string "title", null: false
     t.integer "level", default: 0, null: false
     t.integer "category_id", null: false
-    t.integer "user_id", null: false
+    t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
-    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.boolean "admin", default: false, null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -56,5 +56,5 @@ ActiveRecord::Schema.define(version: 2022_04_05_120532) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
-  add_foreign_key "tests", "users"
+  add_foreign_key "tests", "users", column: "author_id"
 end
