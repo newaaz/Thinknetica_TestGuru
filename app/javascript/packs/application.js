1af07fe8fe9e66cc4e3de38f91e57049b0d41a2d
aaz <ilk-ui@yandex.ru>
2022-05-01 22:40:15 +0300

    add Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,20 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+import 'bootstrap'
+
+import { Tooltip, Popover } from "bootstrap"
+
+document.addEventListener("turbolinks:load", () => {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+      return new Tooltip(tooltipTriggerEl)
+  })
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+      return new Popover(popoverTriggerEl)
+  })
+})
