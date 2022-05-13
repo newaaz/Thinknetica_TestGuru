9d12d9defa22af8cbb58ba47d501e9ed2331eb39
aaz <ilk-ui@yandex.ru>
2022-05-13 10:36:53 +0300

    fix style errors. refactor links to gists

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,13 +1,12 @@
 class GistsController < ApplicationController
   def create
     @test_passage = TestPassage.find params[:test_passage_id]
-
     @result = GistQuestionService.new(@test_passage.current_question).call
 
     if @result.success?
       create_gist
-      flash[:info] = "#{t('.success')} #{view_context.link_to t('.link_to_gist'), @result.url, class: "alert-link",
-                                                                                  target: "_blank", rel: "nofollow noopener" }"
+      
+      flash[:info] = "#{t('.success')} #{view_context.link_to_created_gist(@result.url)}"
     else
       flash[:danger] = t('.failure')
     end
--- app/helpers/gists_helper.rb
+++ app/helpers/gists_helper.rb
@@ -3,7 +3,11 @@ module GistsHelper
     truncate question.body, length: 25
   end
 
-  def url_path(gist)
-    (URI gist.url).path
+  def link_to_gist(gist)
+    link_to (URI gist.url).path, gist.url, target: "_blank", rel: "nofollow noopener"
+  end
+
+  def link_to_created_gist(url)
+    link_to t('gists.create.link_to_gist'), url, class: "alert-link", target: "_blank", rel: "nofollow noopener"
   end
 end
